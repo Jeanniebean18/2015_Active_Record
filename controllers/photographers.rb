fe84eee2fc0c316e4bbd64fceefd830d92e19e40
Jeannie <jeannie@kickpunchcreative.com>
2015-07-16 17:56:07 -0500

    crud

--- controllers/photographers.rb
+++ controllers/photographers.rb
@@ -2,3 +2,23 @@ get "/home" do
   erb :home
 end
 
+get "/create_photographer" do
+  erb :create_photographer
+end
+
+get "/save_photographer" do
+  @p = Photographer.new({name: params["name"]})
+  @p.save
+  erb :photographer_added
+end
+
+get "/show_photographers" do
+  erb :all_photographers
+end
+get "/delete_photographer/:x" do
+  @p = Photographer.find(params[:x])
+  @p.delete
+  erb :photographer_deleted
+end
+
+
